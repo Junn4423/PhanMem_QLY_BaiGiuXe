56f4a9f3a2fb102c64a1e3f3ae6af6c621018c69
CedricDev.44 <115161648+Junn4423@users.noreply.github.com>
2025-05-24 23:22:47 +0700

    Update url.py

--- server/url.py
+++ server/url.py
@@ -1,5 +1,5 @@
 #THIẾT BỊ VÀ CAMERA PHẢI CÙNG 1 MẠNG LAN
-rtsp_url_in = "rtsp://admin:sof@sof.vn@192.168.0.112:554/h264/ch1/main/av_stream" #vao
-rtsp_url_out = "rtsp://admin:sof@sof.vn@192.168.0.113:554/h264/ch1/main/av_stream" #ra
-api_BienSo = "http://192.168.1.20:8000/detect"
-url_api = "http://192.168.0.19/parkinglot/services.sof.vn/index.php"
+rtsp_url_in = "xxx" #vao
+rtsp_url_out = "xxx" #ra
+api_BienSo = "xxx"
+url_api = "xxx"
